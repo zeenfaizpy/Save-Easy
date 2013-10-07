09fc18261ebfb61e8dcbd068179bb5ba081d56ce
zeenfaizpy <zeenfaiz@gmail.com>
2013-10-07 23:45:43 +0530

    changes

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 function FormCtrl($scope)
 {
+	var storage_id = "save_easy";
 	$scope.tasks = [];
+	$scope.perst_tasks = JSON.parse(localStorage.getItem(storage_id));
 	$scope.submit = function()
 	{
 		$scope.tasks.push({description:$scope.description,date:$scope.date,amount:$scope.amount,category:$scope.category});
+		localStorage.setItem(storage_id, JSON.stringify($scope.tasks));
 		$scope.showstate = true;
 		$scope.description = '';
 		$scope.date = '';
 		$scope.amount = '';
 		$scope.category = '';
 	}
-	$scope.showstate = false;
+
+	if($scope.perst_tasks.length > 0)
+	{
+		$scope.showstate = true;
+	}
+	else
+	{
+		$scope.showstate = false;	
+	}
+	
+
 }
